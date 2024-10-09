f95e1a0b1e79605c8481b5ad44fb7d3d6c70868e
0WAQ <2758282037@qq.com>
2024-10-09 23:23:13 +0800

    初始化项目

---
+++ build/source/boot/CMakeFiles/boot.dir/boot.c.obj.d
@@ -0,0 +1,4 @@
+source/boot/CMakeFiles/boot.dir/boot.c.obj: \
+ /home/bwb/work/diy-x86os/start/start/source/boot/boot.c \
+ /usr/include/stdc-predef.h \
+ /home/bwb/work/diy-x86os/start/start/source/boot/boot.h
---
+++ build/source/boot/CMakeFiles/boot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boot.dir/boot.c.obj"
+  "CMakeFiles/boot.dir/boot.c.obj.d"
+  "CMakeFiles/boot.dir/start.S.obj"
+  "boot"
+  "boot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/boot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
