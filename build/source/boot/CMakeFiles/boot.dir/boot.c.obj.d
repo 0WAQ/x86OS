d9e8e5328e7e0ec53d99d9f2abb8175bd92f0ba1
0WAQ <2758282037@qq.com>
2024-10-09 23:24:40 +0800

    删除build/

--- build/source/boot/CMakeFiles/boot.dir/boot.c.obj.d
+++
@@ -1,4 +0,0 @@
-source/boot/CMakeFiles/boot.dir/boot.c.obj: \
- /home/bwb/work/diy-x86os/start/start/source/boot/boot.c \
- /usr/include/stdc-predef.h \
- /home/bwb/work/diy-x86os/start/start/source/boot/boot.h
