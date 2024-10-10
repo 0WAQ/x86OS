a0d3e7a0acbc1ccd17a226badf6b10a19900dd92
0WAQ <2758282037@qq.com>
2024-10-10 17:30:35 +0800

    新增loader模块

--- source/boot/boot.c
+++ source/boot/boot.c
@@ -9,11 +9,15 @@ __asm__(".code16gcc");
 
 #include "boot.h"
 
+// loader加载的地址
+#define LOADER_START_ADDR   0x8000
+
 /**
  * Boot的C入口函数
  * 只完成一项功能，即从磁盘找到loader文件然后加载到内容中，并跳转过去
  */
-void boot_entry(void) {
-
+void boot_entry() {
+    // 将0x8000这个指针转换为 void (*)(), 并调用
+    ((void (*)())LOADER_START_ADDR)();
 } 
 
---
+++ source/loader/loader.h
@@ -0,0 +1,9 @@
+/**
+ *
+ * 二级加载部分，用于实现更为复杂的初始化、内核加载的工作。
+ *
+ */
+#ifndef LOADER_H
+#define LOADER_H
+
+#endif  // LOADER_H
