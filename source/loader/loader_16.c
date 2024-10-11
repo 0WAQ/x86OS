c47a2ab467139d3a7a70832f8b801b09756ad7d7
0WAQ <2758282037@qq.com>
2024-10-11 12:49:29 +0800

    增加显示字符串函数

--- source/loader/loader_16.c
+++ source/loader/loader_16.c
@@ -8,6 +8,24 @@
 // 16位代码，必须加上放在开头，以便有些io指令生成为32位
 __asm__(".code16gcc");
 
+/**
+ * @brief 显示字符串 
+ */
+static void show_msg(const char* msg) {
+    char c;
+
+    while((c = *msg++) != '\0') {
+        __asm__ __volatile__(
+            "mov $0xe, %%ah""\n\t"
+            "mov %[ch], %%al""\n\t"
+            "int $0x10"
+            :                   // 输出操作数，无
+            : [ch]"r"(c)        // 输入操作数，ch建立关联，"r"表示任意寄存器
+        );
+    }
+}
+
 void loader_entry() {
+    show_msg("...loading...\n\r");
     for(;;) {}
 }
