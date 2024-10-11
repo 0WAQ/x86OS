1b6742799fbf9244b1775e49b6db15e4c219f567
0WAQ <2758282037@qq.com>
2024-10-11 13:08:21 +0800

    新增common模块

---
+++ source/common/boot_info.h
@@ -0,0 +1,30 @@
+/**
+ * 
+ * 系统启动信息
+ * 
+ */
+#ifndef BOOT_INFO_H
+#define BOOT_INFO_H
+
+#include "types.h"
+
+#define BOOT_RAM_REGION_MAX 10  // RAM区的最大数量
+
+/**
+ * @brief 启动信息参数
+ */
+typedef struct _boot_info_t {
+    
+    /**
+     * @brief RAM区信息
+     */
+    struct {
+        uint32_t start; // RAM区起始地址
+        uint32_t size;  // RAM区大小
+    }ram_region_cfg[BOOT_RAM_REGION_MAX];
+    
+    int ram_region_count;
+
+}boot_info_t;
+
+#endif // BOOT_INFO_H 
---
+++ source/common/types.h
@@ -0,0 +1,28 @@
+/**
+ * 
+ * 系统内部的基本数据类型
+ * 
+ */
+#ifndef TYPES_H
+#define TYPES_H
+
+/**
+ * 使用宏处理的原因:
+ * 为避免和newlib库中的定义冲突
+ */
+#ifndef _UINT8_T_DECLARED
+#define _UINT8_T_DECLARED
+typedef unsigned char uint8_t;
+#endif // _UINT8_T_DECLARED
+
+#ifndef _UINT16_T_DECLARED
+#define _UINT16_T_DECLARED
+typedef unsigned short uint16_t;
+#endif // _UINT16_T_DECLARED
+
+#ifndef _UINT32_T_DECLARED
+#define _UINT32_T_DECLARED
+typedef unsigned int uint32_t;
+#endif // _UINT32_T_DECLARED
+
+#endif // TYPES_H
