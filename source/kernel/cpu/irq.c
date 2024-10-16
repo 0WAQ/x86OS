8679549228c601e99f7d98e58286bdf0f9d2bed6
0WAQ <2758282037@qq.com>
2024-10-16 13:55:13 +0800

    完善异常

--- source/kernel/cpu/irq.c
+++ source/kernel/cpu/irq.c
@@ -13,17 +13,18 @@ static gate_desc_t idt_table[IDT_TABLE_SIZE];
 
 void irq_init() {
     for(int i = 0; i < IDT_TABLE_SIZE; i++) {
-        gate_desc_set(idt_table + i, KERNEL_SELECTOR_CS, (uint32_t)default_exception_handler, 
+        gate_desc_set(idt_table + i, KERNEL_SELECTOR_CS, (uint32_t)exception_handler_unknown, 
             GATE_ATTR_TYPE_INTR | GATE_ATTR_P | GATE_ATTR_DPL0 | GATE_ATTR_D);
     }
 
     lidt((uint32_t)idt_table, sizeof(idt_table));
 }
 
-void default_exception_handler_aux(exception_frame_t* frame) {
-    do_default_handler(frame, "unknown exception\n");
-}
-
 void do_default_handler(exception_frame_t* frame, const char* msg) {
     for(;;);
 }
+
+void do_handler_unknown(exception_frame_t* frame) {
+    do_default_handler(frame, "unknown exception\n");
+}
+
--- source/kernel/include/cpu/irq.h
+++ source/kernel/include/cpu/irq.h
@@ -14,18 +14,19 @@
 void irq_init();
 
 /**
- * @brief 默认中断处理程序，在init/start.S中实现
- */
-void default_exception_handler();
-
-/**
- * @brief 默认中断处理程序，C实现
- */
-void default_exception_handler_aux(exception_frame_t* frame);
-
-/**
- * @brief 默认中断处理程序的执行函数
+ * @brief 异常处理程序的执行函数
  */
 void do_default_handler(exception_frame_t* frame, const char* msg);
+
+
+/**
+ * @brief unknown的异常处理程序，在init/start.S中实现
+ */
+void exception_handler_unknown();
+
+/**
+ * @brief unknown的异常处理程序，C实现
+ */
+void do_handler_unknown(exception_frame_t* frame);
 
 #endif // IRQ_H
