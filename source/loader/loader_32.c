f63304878e5e5450287297c9950ba4f5140b3db2
0WAQ <2758282037@qq.com>
2024-10-31 21:57:36 +0800

    修改kernel.lds, 将一号进程的所有段放到虚拟地址0x80000000处

--- source/loader/loader_32.c
+++ source/loader/loader_32.c
@@ -74,21 +74,23 @@ uint32_t reload_elf_file(uint8_t* file_buffer) {
         return 0;
     }
 
-    // 加载程序头，将内容拷贝到相应的位置
+    // 读取Program Header，将内容拷贝到相应的位置
     for(int i = 0; i < elf_hdr->e_phnum; i++) {
+
+        // 文件首字节 + Program Headers Offset是第一个phdr
         Elf32_Phdr* phdr = (Elf32_Phdr*)(file_buffer + elf_hdr->e_phoff) + i;
         if(phdr->p_type != PT_LOAD) {
             continue;
         }
 
-        // 全部使用物理地址
+        // 全部使用物理地址, 此时分页机制还未打开
         uint8_t* src = file_buffer + phdr->p_offset;
         uint8_t* dest = (uint8_t*)phdr->p_paddr;
         for(int j = 0; j < phdr->p_filesz; j++) {
             *dest++ = *src++;
         }
 
-        // memsz和filesz不同时要填0
+        // memsz和filesz不同时要填0(bss段)
         dest = (uint8_t*)phdr->p_paddr + phdr->p_filesz;
         for(int j = 0; j < phdr->p_memsz - phdr->p_filesz; j++) {
             *dest++ = 0;
