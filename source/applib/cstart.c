f7473acb1a7487458d806decc2e6cd9999dd48b7
0WAQ <2758282037@qq.com>
2024-11-13 13:41:49 +0800

    增加crt相关代码, 为exec做前置准备

---
+++ source/applib/cstart.c
@@ -0,0 +1,11 @@
+/**
+ * 
+ * 
+ * 
+ */
+
+int main(int, char**);
+
+void cstart(int argc, char** argv) {
+    main(argc, argv);
+}
---
+++ source/shell/main.c
@@ -0,0 +1,13 @@
+/***
+ * 
+ * 命令行解释器
+ * 
+ */
+
+#include "lib_syscall.h"
+
+int main(int argc, char** argv) {
+    for(;;) {
+        msleep(1000);
+    }
+}
